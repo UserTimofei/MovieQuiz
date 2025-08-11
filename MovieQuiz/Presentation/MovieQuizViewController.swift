92ffed5ec57eafb4da10edb83ce1e1eac7094e50
Timofei <kirichenko.timofei@gmail.com>
2025-08-11 20:22:42 +0500

    Востановнение

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -3,23 +3,50 @@ import UIKit
 final class MovieQuizViewController: UIViewController {
     // MARK: - Lifecycle
     
-    @IBOutlet weak var yesButton: UIButton!
-    @IBOutlet weak var noButton: UIButton!
-    @IBOutlet weak var questionTitleLabel: UILabel!
-    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet private weak var yesButton: UIButton!
+    @IBOutlet private weak var noButton: UIButton!
+    @IBOutlet private weak var questionTitleLabel: UILabel!
+    @IBOutlet private weak var questionLabel: UILabel!
+    @IBOutlet private weak var indexLabel: UILabel!
+    
+    @IBOutlet private weak var imageView: UIImageView!
+    
+    
+    @IBAction private func yesButtonClicked(_ sender: UIButton) {
+    }
+    @IBAction private func noButtonClicked(_ sender: UIButton) {
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        noButton.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
-        yesButton.titleLabel?.font = UIFont(name: "YS Display-Medium", size: 20)
-        questionTitleLabel.font = UIFont(name: "YS Display-Medium", size: 20)
-        questionLabel.font = UIFont(name: "YS Display-Bold", size: 23)
+        noButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        yesButton.titleLabel?.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        indexLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionTitleLabel.font = UIFont(name: "YSDisplay-Medium", size: 20)
+        questionLabel.font = UIFont(name: "YSDisplay-Bold", size: 23)
         questionLabel.textAlignment = .center
         questionLabel.numberOfLines = 2
         
        }
-        
+    private struct QuizQuestion {
+        let image: String
+        let text: String
+        let correctAnswer: Bool
     }
+    private let questions: [QuizQuestion] = [
+        QuizQuestion(image: "The Godfather", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Dark Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Kill Bill", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Avengers", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Deadpool", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "The Green Knight", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: true),
+        QuizQuestion(image: "Old", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "The Ice Age Adventures of Buck Wild", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Tesla", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false),
+        QuizQuestion(image: "Vivarium", text: "Рейтинг этого фильма больше чем 6?", correctAnswer: false)
+    ]
+    
+}
 
 
 /*
